dd2e06930300847da43e1884bb0928a1c1cfca00
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-08-25 02:49:48 +0900

    Time: 67 ms (67.64%), Space: 46 MB (83.36%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -1,44 +1,12 @@
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
 var canJump = function(nums) {
-  let zero = 0;
-
-  if (nums.length === 1) {
+    // Keep track of max distance traveled so far
+    let max = 0;
+    for(let i=0;i<nums.length;i++){
+        // The only time that max < i is when we are at 0 and we cannot move forward
+        if(i>max) return false;
+        // Move the max pointer the maximum 
+        max = Math.max(nums[i]+i,max);
+    }
+    // If we did not get stuck at a 0 then return true
     return true;
-  }
-
-  for (let i = 0; i < nums.length; i++) {
-    console.log("cur:", nums[i]);
-    if (nums[i] === 0) {
-      // 0이면 zero 증가
-      zero++;
-    }
-
-    if (zero && nums[i + 1] !== 0) {
-      let j = i - zero;
-
-      while (j >= -1) {
-        console.log("cur:", nums[i], "back:", nums[j], "zero:", zero);
-        // 위치에 0이 있으면 return false
-        // if (nums[j] === 0 || j === -1)
-        if (j === -1)
-          return false;
-
-        if (nums[j] > zero || j + nums[j] === nums.length - 1) {
-          console.log(j + zero);
-          // zero 전 위치에 숫자가 zero보다 커야함
-          zero = 0;
-          break;
-        } else  {
-          // zero 전 위치의 숫자가 zero보다 작다면 zero++ j--
-          zero++;
-          j--;
-        }
-      }
-    }
-  }
-
-  return true;
 };
