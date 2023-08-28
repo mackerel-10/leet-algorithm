9b3e5a43b3b621a95a7e3816f134a01a0491daf9
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-08-28 15:12:34 +0900

    Time: 61 ms (50.08%), Space: 43.2 MB (17.88%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  // alreday sorted in non-decreasing order
+  // 1 <= index1 < index2 < numbers.length
+  // return indices of the two numbers
+  let i = 0, j = 1;
+
+  if (numbers.length === 0) {
+    return;
+  }
+
+  while (i < numbers.length - 1) {
+    if (j === numbers.length) {
+      i++;
+      j = i + 1;
+      continue;
+    }
+
+    if (numbers[i] + numbers[j] !== target) {
+      if (numbers[i] === numbers[i + 1]) {
+        i++;
+      } else {
+        j++;
+      }
+    } else {
+      return [i + 1, j + 1];
+    }
+  }
+};
