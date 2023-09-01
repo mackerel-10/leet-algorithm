60fd80083bb0901457736019c4107c65aa43ef61
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-09-01 10:05:10 +0900

    Time: 95 ms (71.79%), Space: 66.4 MB (38.28%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -7,24 +7,14 @@ var containsNearbyDuplicate = function(nums, k) {
   let hashTable = {};
   let j;
 
+  const hi = new Map();
   for (let i = 0; i < nums.length; i++) {
-    j = hashTable[nums[i]];
-
-    if (j !== undefined && i - j <= k) {
+    
+    if (hi.has(nums[i]) && i - hi.get(nums[i])<= k) {
       return true;
     }
-    hashTable[nums[i]] = i;
-  }
+    hi.set(nums[i],i)
+ // console.log(hi.set(nums[i],i));
+  }  
   return false;
-
-//   const hi = new Map();
-//   for (let i = 0; i < nums.length; i++) {
-    
-//     if (hi.has(nums[i]) && i - hi.get(nums[i])<= k) {
-//       return true;
-//     }
-//     hi.set(nums[i],i)
-//  // console.log(hi.set(nums[i],i));
-//   }  
-//   return false;
 };
