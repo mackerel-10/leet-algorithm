51d7db5f8a74237bb36a27e025356b59be80b21e
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-09-15 01:08:51 +0900

    Time: 60 ms (36.92%), Space: 43.7 MB (61.64%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.js
+++ 0133-clone-graph/0133-clone-graph.js
@@ -10,25 +10,19 @@
  * @param {Node} node
  * @return {Node}
  */
-var cloneGraph = function(node) {
-    if(node == null) return node;
-    const visited = new Map();
-    return cloneHelper(node,visited);
-};
 
-var cloneHelper = function(node,visited){
-    const newNode =new Node(node.val);
-    visited.set(node.val, newNode);
-
-    for(const neighbor of node.neighbors){
-        if(!visited.has(neighbor.val)){
-            const newNeighbor = cloneHelper(neighbor,visited);
-            newNode.neighbors.push(newNeighbor);
-        }
-        else{
-            const newNeighbor = visited.get(neighbor.val);
-            newNode.neighbors.push(newNeighbor);
-        }
+var cloneGraph = function(node, copy = new Map()) {
+    // Handle null case
+    if (!node) return null;
+        
+    // If our graph hasn't already copied the input node
+    if (!copy.has(node.val)) {
+        // Create new copy node (+ reference in map)
+        copy.set(node.val, new Node(node.val));
+        // Recursively clone neighbors
+        copy.get(node.val).neighbors = node.neighbors.map((neighbor) => cloneGraph(neighbor, copy));
     }
-    return newNode;
-}
+        
+    // Return copied/new node (* not * the original node passed in)
+    return copy.get(node.val);
+};
